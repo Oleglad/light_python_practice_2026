9207e7b8d25213e16918ff40924866ff8f679902
Oleglad <tamerlan2802@gmail.com>
2026-09-11 00:58:54 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,11 @@
+import os
 
+folder_path = input("Введите путь к папке: ").strip()
+
+# Убираем кавычки, если пользователь их поставил
+folder_path = folder_path.strip('"\'')
+
+if os.path.exists(folder_path):
+    print(f"Принимаю папку: {folder_path}")
+else:
+    print("Такой папки нет, попробуйте снова")
